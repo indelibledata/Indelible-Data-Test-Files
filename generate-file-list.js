2a1eb55269a071d513f97af7050763da64639681
Indelible Data <159039544+indelibledata@users.noreply.github.com>
2024-05-15 14:23:22 +0100

    Update generate-file-list.js

--- generate-file-list.js
+++ generate-file-list.js
@@ -5,13 +5,16 @@ const username = 'indelibledata';
 const repo = 'Indelible-Data-Test-Files';
 const apiUrl = `https://api.github.com/repos/${username}/${repo}/contents?ref=main`;
 
+// Directories and files to ignore
+const ignoreList = ['.github', '.gitattributes', 'README.md', 'generate-file-list.js'];
+
 async function getFileList() {
   try {
     const response = await axios.get(apiUrl);
     const fileList = {};
 
     for (const item of response.data) {
-      if (item.type === 'dir') {
+      if (item.type === 'dir' && !ignoreList.includes(item.name)) {
         const directoryUrl = `${apiUrl}/${item.name}`;
         const directoryResponse = await axios.get(directoryUrl);
         const files = directoryResponse.data.map(file => file.name);
